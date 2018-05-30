6d3e08cd9c533d5dd4d81d3fdeaa5d664ab37c64
Andrew <31520558+natarian@users.noreply.github.com>
2018-05-30 12:12:27 -0400

    updated sandbox files

--- sandbox/controller_test.js
+++ sandbox/controller_test.js
@@ -1,6 +1,27 @@
 (function(angular) {
+
   var app = angular.module('myApp', []);
-  app.controller('FirstCtrl', function($scope) {
-    $scope.data = {message: "Hello"};
+
+  app.factory('Data', function() {
+    return {message:"I'm data from a service"}
+  })
+
+app.filter('reverse', function (Data) {
+  return function (text) {
+    return text.split("").reverse().join("") + Data.message;
+  }
+})
+
+  app.controller('FirstCtrl', function($scope, Data) {
+    $scope.data = Data;
   });
+
+  app.controller('SecondCtrl', function($scope, Data) {
+    $scope.data = Data;
+
+    $scope.reversedMessage = function (message) { /*defining a method on a scope*/
+      return message.split("").reverse().join("");
+    }
+  });
+
 }(angular));
